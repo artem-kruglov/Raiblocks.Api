b1bbe3a02f6013bc5ab415f90f37a9c1ef7133cd
BorisKarabanov <bkarabanov@ics.perm.ru>
2018-02-07 15:43:33 +0500

    Some comments and config stub

--- Lykke.Service.Raiblocks.Api/src/Lykke.Service.RaiblocksApi.Core/Repositories/Addresses/IAddressHistoryEntryRepository.cs
+++ Lykke.Service.Raiblocks.Api/src/Lykke.Service.RaiblocksApi.Core/Repositories/Addresses/IAddressHistoryEntryRepository.cs
@@ -9,6 +9,14 @@ namespace Lykke.Service.RaiblocksApi.Core.Repositories.Addresses
     public interface IAddressHistoryEntryRepository<HistoryEntry> : IRepository<HistoryEntry>
         where HistoryEntry : IAddressHistoryEntry
     {
+        /// <summary>
+        /// Return history entries for address after specific hash
+        /// </summary>
+        /// <param name="take">Amount of the returned history entry</param>
+        /// <param name="partitionKey">PartitionKey for azure table storage</param>
+        /// <param name="address">Address</param>
+        /// <param name="afterBlockCount">Block hash</param>
+        /// <returns>History entries for address after specific hash</returns>
         Task<IEnumerable<HistoryEntry>> GetByAddressAsync(int take, string partitionKey, string address, long afterBlockCount = 0);
     }
 }
--- Lykke.Service.Raiblocks.Api/src/Lykke.Service.RaiblocksApi.Core/Services/IBlockchainService.cs
+++ Lykke.Service.Raiblocks.Api/src/Lykke.Service.RaiblocksApi.Core/Services/IBlockchainService.cs
@@ -9,15 +9,63 @@ namespace Lykke.Service.RaiblocksApi.Core.Services
 {
     public interface IBlockchainService
     {
+        /// <summary>
+        /// Get balances for addresses
+        /// </summary>
+        /// <param name="addresses">Addresses</param>
+        /// <returns>Balances for addresses</returns>
         Task<Dictionary<string, string>> GetAddressBalances(IEnumerable<string> addresses);
 
+        /// <summary>
+        /// Check validity for address
+        /// </summary>
+        /// <param name="address">Addresse</param>
+        /// <returns>Validity</returns>
         Task<bool> IsAddressValidAsync(string address);
 
+        /// <summary>
+        /// Build unsined transaction
+        /// </summary>
+        /// <param name="address">Address from</param>
+        /// <param name="destination">Address to</param>
+        /// <param name="amount">Amount</param>
+        /// <returns>Unsined transaction</returns>
         Task<string> CreateUnsignSendTransaction(string address, string destination, string amount);
+
+        /// <summary>
+        /// Get balance for address
+        /// </summary>
+        /// <param name="address">Address</param>
+        /// <returns>Balance</returns>
         Task<string> GetAddressBalanceAsync(string address);
+
+        /// <summary>
+        /// Get address chain height
+        /// </summary>
+        /// <param name="address">Address</param>
+        /// <returns>Block count</returns>
         Task<Int64> GetAddressBlockCountAsync(string address);
+
+        /// <summary>
+        /// Broadcast transaction to network
+        /// </summary>
+        /// <param name="signedTransaction">Signed transaction</param>
+        /// <returns>Broadcast result (hash or error)</returns>
         Task<(string hash, string error)> BroadcastSignedTransactionAsync(string signedTransaction);
+
+        /// <summary>
+        /// Get address history
+        /// </summary>
+        /// <param name="address">Address</param>
+        /// <param name="take">Amount of the returned history entries</param>
+        /// <returns>Account history</returns>
         Task<IEnumerable<(string from, string to, BigInteger amount, string hash)>> GetAddressHistoryAsync(string address, int take);
+
+        /// <summary>
+        /// Get address info
+        /// </summary>
+        /// <param name="address">Address</param>
+        /// <returns>Return frontier and block count</returns>
         Task<(string frontier, long blockCount)> GetAddressInfoAsync(string address);
     }
 }
